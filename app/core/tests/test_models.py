46d784e4924b45d39748ad963a48840d8e71884f
Smilexix_ <ayon000777@gmail.com>
2022-02-08 22:25:46 +0600

    Add validation for email field

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -23,3 +23,9 @@ def test_new_user_email_normalized(self):
     user = get_user_model().objects.create_user(email, 'test123')
 
     self.assertEqual(user.email, email.lower())
+
+
+def test_new_user_invalid_email(self):
+    """Test creating user with no email raises error"""
+    with self.assertRaises(ValueError):
+        get_user_model().objects.create_user(None, 'test123')
